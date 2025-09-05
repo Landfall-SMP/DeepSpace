7b96d1a76754951250cb89649811593943ae7ff0
mallowwww <mallowhorton@proton.me>
2025-09-05 10:35:38 -0500

    Angel blocks

--- src/main/java/world/landfall/deepspace/ModBlocks.java
+++ src/main/java/world/landfall/deepspace/ModBlocks.java
@@ -1,11 +1,14 @@
 package world.landfall.deepspace;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Deepspace.MODID);
-
+    public static final DeferredBlock<Block> ANGEL_BLOCK = BLOCKS.register("angel_block", () -> new Block(BlockBehaviour.Properties.of()));
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
--- src/main/java/world/landfall/deepspace/ModItems.java
+++ src/main/java/world/landfall/deepspace/ModItems.java
@@ -1,11 +1,14 @@
 package world.landfall.deepspace;
 
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import world.landfall.deepspace.item.AngelBlockItem;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Deepspace.MODID);
-
+    public static final DeferredItem<Item> ANGEL_BLOCK_ITEM = ITEMS.register("angel_block", () -> new AngelBlockItem(new Item.Properties()));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
