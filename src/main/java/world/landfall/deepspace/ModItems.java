b29a4f9efbbd9bf0d92341f6f8dcff7c6267df47
mallowwww <mallowhorton@proton.me>
2025-09-21 00:31:29 -0500

    Add rocket boosters

--- src/main/java/world/landfall/deepspace/ModItems.java
+++ src/main/java/world/landfall/deepspace/ModItems.java
@@ -8,12 +8,14 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import world.landfall.deepspace.item.AngelBlockItem;
 import world.landfall.deepspace.item.JetHelmetItem;
 import world.landfall.deepspace.item.JetpackItem;
+import world.landfall.deepspace.item.RocketBoosterItem;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Deepspace.MODID);
     public static final DeferredItem<Item> ANGEL_BLOCK_ITEM = ITEMS.register("angel_block", () -> new AngelBlockItem(new Item.Properties()));
     public static final DeferredItem<JetpackItem> JETPACK_ITEM = ITEMS.register("jetpack", JetpackItem::new);
     public static final DeferredItem<JetHelmetItem> JET_HELMET_ITEM = ITEMS.register("jet_helmet", JetHelmetItem::new);
+    public static final DeferredItem<RocketBoosterItem> ROCKET_BOOSTER_ITEM = ITEMS.register("rocket_booster", RocketBoosterItem::new);
     public static void register(IEventBus eventBus) {
         JetpackItem.JetpackComponent.register(eventBus);
         JetHelmetItem.JetHelmetComponent.register(eventBus);
