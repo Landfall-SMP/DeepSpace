7633c307bb44ce87fd67849f714f0705fcdd8349
mallowwww <mallowhorton@proton.me>
2025-11-14 10:45:54 -0600

    Proper textures for the building blocks

--- src/main/java/world/landfall/deepspace/ModBlocks.java
+++ src/main/java/world/landfall/deepspace/ModBlocks.java
@@ -8,6 +8,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import world.landfall.deepspace.block.AngelBlock;
+import world.landfall.deepspace.block.LunarSoilBlock;
 import world.landfall.deepspace.block.MoonstoneZincOreBlock;
 import world.landfall.deepspace.block.OxygenatorBlock;
 
@@ -16,11 +17,11 @@ public class ModBlocks {
     public static final DeferredBlock<Block> ANGEL_BLOCK = BLOCKS.register("angel_block", () -> new AngelBlock(BlockBehaviour.Properties.of()));
     public static final DeferredBlock<Block> OXYGENATOR_BLOCK = BLOCKS.register("oxygenator", OxygenatorBlock::new);
     public static final DeferredBlock<Block> MOONSTONE_ZINC_ORE_BLOCK = BLOCKS.register("moonstone_zinc_ore_block", MoonstoneZincOreBlock::new);
-    public static final DeferredBlock<Block> LUNAR_SOIL = BLOCKS.register("lunar_soil", () -> new Block(BlockBehaviour.Properties.of()
+    public static final DeferredBlock<Block> LUNAR_SOIL = BLOCKS.register("lunar_soil", () -> new LunarSoilBlock(BlockBehaviour.Properties.of()
             .strength(1, 1)
             .sound(SoundType.SAND)
-            .friction(2)
             .mapColor(MapColor.COLOR_LIGHT_GRAY)
+            .speedFactor(.7f)
     ));
     public static final DeferredBlock<Block> LUNAR_COBBLE = BLOCKS.register("lunar_cobble", () -> new Block(BlockBehaviour.Properties.of()
             .strength(4, 3)
